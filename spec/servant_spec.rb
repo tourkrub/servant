c45f83d7b6fb0f8d4c6b5c1b00846a16adfc8fdd
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-04-10 10:51:41 +0700

    Update rspec

--- spec/servant_spec.rb
+++ spec/servant_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Servant do
   it "has a version number" do
     expect(Servant::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end

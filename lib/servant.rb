6dfa8b753f7c48c2156e0c0a768fb0d9c31badec
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-04-10 16:30:49 +0700

    Add publisher

--- lib/servant.rb
+++ lib/servant.rb
@@ -1,6 +1,7 @@
 require "servant/version"
 require "servant/logger"
 require "servant/subscriber"
+require "servant/publisher"
 
 require "servant/service/base_service"
 require "servant/service/pipedrive/create_deal_service"

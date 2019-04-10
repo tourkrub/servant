6b00e259539399dbcceb1399cd4b4eb812ba3727
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-04-10 11:09:48 +0700

    Add subscriber spec

---
+++ spec/unit/subscriber_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe Servant::Subscriber do
+  describe "#call_event_handler" do
+    it "call method from provided constant" do
+      class FooEvent < BaseEvent 
+        def bar
+          message["response"]
+        end
+      end
+
+      dump_message = JSON.dump({response: "OK"})
+      expect(Servant::Subscriber.new.send(:call_event_handler, "foo.bar", dump_message)).to eq("OK")
+    end
+  end
+end

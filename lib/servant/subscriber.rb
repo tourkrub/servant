52cf5373c60031dc230451e2965706a9d6647c8c
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-04-17 09:57:44 +0700

    Update subscribe to handle group consumer

--- lib/servant/subscriber.rb
+++ lib/servant/subscriber.rb
@@ -1,3 +1,5 @@
+require "byebug"
+require "securerandom"
 require "redis"
 
 module Servant
@@ -8,16 +10,8 @@ module Servant
 
     def initialize
       @connection = Redis.new(host: REDIS_HOST)
-      # @channels = []
+      @channels = []
     end
-
-    # def publish(event: "default", message:)
-    #   connection.publish(event, message)
-    # end
-
-    # def subscribe(event)
-    #   channels.push(event)
-    # end
 
     def process
       Servant.logger.info("Application started")
@@ -26,16 +20,46 @@ module Servant
         exit # rubocop:disable Rails/Exit
       end
 
-      connection.psubscribe("*") do |on|
-        on.pmessage do |channel, event, message|
-          Servant.logger.info "Received event - #{event} from #{channel}"
+      group_id = "group-1"
+      consumer_id = SecureRandom.uuid
+      stream_keys = %w(order.created deal.created).freeze
+      @last_ids = {}
+
+      stream_keys.each do |key|
+        @last_ids[key] = ">"
+        connection.xgroup(:create, key, group_id, '$')
+      rescue
+        nil
+      end
+
+      while true do
+        event = connection.xreadgroup(group_id, consumer_id, stream_keys, @last_ids.values, block: 100000)
+
+        if event.empty?
+          next
+        end
+
+        event_name = event.keys[0]
+
+        if event.values[0].empty?
+          @last_ids[event_name] =  ">"
+          next
+        end
+        
+        event_id, message = event[event_name][0][0], event[event_name][0][1]["message"]
+
+        begin
+          Servant.logger.info "Received event - #{event_name}"
           time_start = Time.now.to_f
-          call_event_handler(event, message)
+          sleep(rand(0.1...0.9))
+          call_event_handler(event_name, message)
         rescue Exception => e # rubocop:disable Lint/RescueException
           Servant.logger.fatal e.class.name
           Servant.logger.fatal e.message
           Servant.logger.fatal e.backtrace
         ensure
+          @last_ids[event_name] = event_id
+
           time_diff = (Time.now.to_f - time_start).round(3)
           Servant.logger.info "Completed in #{time_diff}s"
         end

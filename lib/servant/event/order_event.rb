8fbc7604ddf2d2d431d8d3fd24f3702824b08460
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-04-18 10:00:16 +0700

    Remove not needed code

--- lib/servant/event/order_event.rb
+++ lib/servant/event/order_event.rb
@@ -1,5 +1,5 @@
 class OrderEvent < BaseEvent
   def created
-    Pipedrive::CreateDealService.process(order_id: message["order_id"])
+    # Pipedrive::CreateDealService.process(order_id: message["order_id"])
   end
 end

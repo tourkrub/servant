0f65d37446dbc532afa874ef27e0448168d6f9a4
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-04-23 15:26:25 +0700

    Remove event

--- lib/servant/event/deal_event.rb
+++ lib/servant/event/deal_event.rb
@@ -1,5 +1,5 @@
-class DealEvent < BaseEvent
-  def created
-    Slack::SendNotificationService.process(deal_id: message["deal_id"], template: "check_availability")
-  end
-end
+# class DealEvent < BaseEvent
+#   def created
+#     Slack::SendNotificationService.process(deal_id: message["deal_id"], template: "check_availability")
+#   end
+# end

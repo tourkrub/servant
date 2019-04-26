225bfff842288bfe7e32ad2ece23a0adf511aab9
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-04-26 15:23:54 +0700

    Fix rubocop

--- lib/servant/cli.rb
+++ lib/servant/cli.rb
@@ -25,7 +25,7 @@ module Servant
       begin
         require "#{Dir.pwd}/config/servant.rb"
       rescue LoadError
-        # LoadError
+        nil
       end
       require_relative "../servant"
 

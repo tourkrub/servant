dc2d0d95016d269db332e884e0b4e445e2078550
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-05-13 17:28:26 +0700

    Test #send o event_handler

--- spec/unit/servant/event_handler_spec.rb
+++ spec/unit/servant/event_handler_spec.rb
@@ -1,11 +1,15 @@
 RSpec.describe Servant::EventHandler do
   describe "#send" do
     class TestServantEventHandlerFoo < Servant::EventHandler
+      def bar
+        event
+      end
     end
 
     it "intialize with event, message and meta" do
       instance = TestServantEventHandlerFoo.new(event: "foo", message: "bar", meta: "baz")
-
+      
+      expect(instance.send("bar")).to eq("foo")
       expect(instance.event).to eq("foo")
       expect(instance.message).to eq("bar")
       expect(instance.meta).to eq("baz")

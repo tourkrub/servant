7d59b99d3de59f291c7b3bace44d3b22e2e15407
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-05-13 17:17:37 +0700

    Add servant publisher spec

---
+++ spec/unit/servant/event_publisher_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Servant::Publisher do
+  describe "#publish" do
+    it "return valid response" do
+      event_id = Servant::Publisher.client.publish(event: "foo", message: {foo: "bar"}, meta: {bar: "baz"})
+
+      expect(event_id).not_to be_empty
+    end
+  end
+end

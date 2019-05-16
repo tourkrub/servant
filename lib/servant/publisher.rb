468ebeca3960b2a7e895cea52fb4a68710d28509
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-05-16 14:15:44 +0700

    Add maxlen to pusblisher

--- lib/servant/publisher.rb
+++ lib/servant/publisher.rb
@@ -11,7 +11,7 @@ module Servant
     end
 
     def publish(event:, message:, meta: {})
-      connection.xadd("event:#{event}", message: JSON.dump(message), meta: JSON.dump(meta))
+      connection.xadd("event:#{event}", {message: JSON.dump(message), meta: JSON.dump(meta)}, maxlen: 1000)
     end
 
     class << self

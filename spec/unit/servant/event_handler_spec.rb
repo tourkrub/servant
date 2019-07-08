d448e955d8a51d6cc6a263cd22446d74ffe780a5
Sittitep Tosuwan <sittitep.tos@tourkrub.co>
2019-07-08 13:15:31 +0700

    Remove tk toolkit from gemspec

--- spec/unit/servant/event_handler_spec.rb
+++ spec/unit/servant/event_handler_spec.rb
@@ -1,47 +1,72 @@
 RSpec.describe Servant::EventHandler do
-  describe "#send" do
-    class TestServantEventHandlerFoo < Servant::EventHandler
-      set_async_methods :foo
+  context "tourkrub-toolkit is not installed" do
+    describe "define an EventHandler with set_async_methods" do
+      it "should raise error" do
+        allow(Object).to receive(:const_defined?).with("Sidekiq").and_return(true)
+        allow(Object).to receive(:const_defined?).with("Tourkrub::Toolkit::AsyncMethod").and_return(false)
 
-      def bar
-        event
+        expect{
+          class TestServantEventHandlerBar < Servant::EventHandler
+            set_async_methods :bar
+
+            def bar
+              event
+            end
+          end
+        }.to raise_error("Tourkrub::Toolkit::AsyncMethod is requried")
       end
+    end
+  end
 
-      def foo
-        event
+  context "tourkrub-toolkit is installed" do
+    before do
+      require 'tourkrub/toolkit'
+
+      class TestServantEventHandlerFoo < Servant::EventHandler
+        set_async_methods :foo
+  
+        def bar
+          event
+        end
+  
+        def foo
+          event
+        end
       end
     end
 
-    it "after_intialize with event, message and meta" do
-      instance = TestServantEventHandlerFoo.new
-
-      instance.instance_variable_set("@on", "foo")
-      instance.instance_variable_set("@request", "message" => { foo: "bar" }, "meta" => { bar: "baz" })
-      instance.after_initialize
-
-      expect(instance.send("bar")).to eq("foo")
-      expect(instance.event).to eq("foo")
-      expect(instance.message).to eq(foo: "bar")
-      expect(instance.meta).to eq(bar: "baz")
-
-      instance.send("foo")
-
-      expect(FooTestServantEventHandlerFooWorker.jobs.size).to eq(1)
-    end
-
-    it "track metrics" do
-      instance = TestServantEventHandlerFoo.new
-
-      allow(instance)
-        .to receive(:metric_agent)
-        .and_return(Servant::Metrics::NullStrategy.new)
-
-      expect(instance)
-        .to receive(:increment_matric)
-        .with("Custom/Events/bar")
-
-      instance.instance_variable_set("@event", "bar")
-      instance.send("bar")
+    describe "#send" do
+      it "after_intialize with event, message and meta" do
+        instance = TestServantEventHandlerFoo.new
+  
+        instance.instance_variable_set("@on", "foo")
+        instance.instance_variable_set("@request", "message" => { foo: "bar" }, "meta" => { bar: "baz" })
+        instance.after_initialize
+  
+        expect(instance.send("bar")).to eq("foo")
+        expect(instance.event).to eq("foo")
+        expect(instance.message).to eq(foo: "bar")
+        expect(instance.meta).to eq(bar: "baz")
+  
+        instance.send("foo")
+  
+        expect(FooTestServantEventHandlerFooWorker.jobs.size).to eq(1)
+      end
+  
+      it "track metrics" do
+        instance = TestServantEventHandlerFoo.new
+  
+        allow(instance)
+          .to receive(:metric_agent)
+          .and_return(Servant::Metrics::NullStrategy.new)
+  
+        expect(instance)
+          .to receive(:increment_matric)
+          .with("Custom/Events/bar")
+  
+        instance.instance_variable_set("@event", "bar")
+        instance.send("bar")
+      end
     end
   end
 end
